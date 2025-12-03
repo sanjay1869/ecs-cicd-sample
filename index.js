bc53e927aa5ff31d7642d91f1c22aeab7f3f4e9a
Sanjay Sharma~ <sanjay.delhi18@gmail.com>
2025-12-03 15:01:56 +0530

    Initial app

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const port = process.env.PORT || 8080;
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type':'text/plain'});
+  res.end('Hello from ECS Fargate!\\n');
+});
+server.listen(port, ()=>console.log('listening on', port));
+
